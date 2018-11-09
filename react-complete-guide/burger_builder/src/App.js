1b99617746dbd6626defb3997ac4bd3e8d90f756
Yvan Cottet <yvan@esl-education.org>
2018-11-09 17:06:18 +0100

    backup after tests

--- react-complete-guide/burger_builder/src/App.js
+++ react-complete-guide/burger_builder/src/App.js
@@ -4,11 +4,19 @@ import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const AsyncCheckout = asyncComponent(() => {
+	return import('./containers/Checkout/Checkout');
+});
+const AsyncOrders = asyncComponent(() => {
+	return import('./containers/Orders/Orders');
+});
+const AsyncAuth = asyncComponent(() => {
+	return import('./containers/Auth/Auth');
+});
 
 class App extends Component {
 
@@ -19,7 +27,7 @@ class App extends Component {
 	render() {
 		let routes = (
 			<Switch>
-				<Route path="/auth" component={Auth} />
+				<Route path="/auth" component={AsyncAuth} />
 				<Route path="/" exact component={BurgerBuilder} />
 				<Redirect to="/" />
 			</Switch>
@@ -28,9 +36,10 @@ class App extends Component {
 		if (this.props.isAuthenticated) {
 			routes = (
 				<Switch>
-					<Route path="/checkout" component={Checkout} />
-					<Route path="/orders" component={Orders} />
+					<Route path="/checkout" component={AsyncCheckout} />
+					<Route path="/orders" component={AsyncOrders} />
 					<Route path="/logout" component={Logout} />
+					<Route path="/auth" component={AsyncAuth} />
 					<Route path="/" exact component={BurgerBuilder} />
 					<Redirect to="/" />
 				</Switch>
--- react-complete-guide/burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-complete-guide/burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import withErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler';
 import axios from '../../axios-order';
 import * as actions from '../../store/actions/index';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
 
 	state = {
 		purchasing: false,
